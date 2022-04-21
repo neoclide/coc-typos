9ac9a97dd84ad2e9470036b03a156d5a5586e1fb
Qiming Zhao <chemzqm@gmail.com>
2022-04-22 00:38:57 +0800

    use readline for better performance

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,7 @@
 import { spawn } from 'child_process'
 import { Disposable, window } from 'coc.nvim'
 import debounce from 'debounce'
+import readline from 'readline'
 import fs from 'fs'
 
 export interface TyposItem {
@@ -12,23 +13,23 @@ export interface TyposItem {
   corrections: string[]
 }
 
-export function spawnCommand(cmd: string, args: string[], stdin: string): Promise<string | undefined> {
+export function spawnCommand(cmd: string, args: string[], stdin: string, onLine: (line: string) => void): Promise<void> {
   const cp = spawn(cmd, args, { cwd: process.cwd(), serialization: 'advanced' })
-  let res = ''
   return new Promise((resolve, reject) => {
     cp.on('error', (err) => {
       reject(err)
     })
+    const rl = readline.createInterface(cp.stdout)
+    rl.on('line', line => {
+      onLine(line)
+    })
+    rl.on('close', () => {
+      resolve()
+    })
     cp.stdin.write(stdin)
     cp.stdin.end()
-    cp.stdout.on('data', data => {
-      res += data.toString()
-    })
     cp.stderr.on('data', data => {
       window.showErrorMessage(`"${cmd} ${args.join(' ')}" error: ${data.toString()}`)
-    })
-    cp.on('close', code => {
-      resolve(res)
     })
   })
 }
@@ -36,27 +37,23 @@ export function spawnCommand(cmd: string, args: string[], stdin: string): Promis
 export function getTypos(cmd: string, content: string): Promise<ReadonlyArray<TyposItem>> {
   let res: TyposItem[] = []
   return new Promise((resolve, reject) => {
-    spawnCommand(cmd, ['--format=json', '-'], content).then(text => {
-      if (text) {
-        let lines = text.split(/\r?\n/)
-        for (let line of lines) {
-          if (line.length) {
-            try {
-              let obj = JSON.parse(line)
-              res.push({
-                type: obj.type,
-                word: obj.typo,
-                lnum: obj.line_num - 1,
-                colStart: obj.byte_offset,
-                colEnd: obj.byte_offset + Buffer.byteLength(obj.typo),
-                corrections: obj.corrections
-              })
-            } catch (e) {
-              // ignored
-            }
-          }
+    spawnCommand(cmd, ['--format=json', '-'], content, line => {
+      if (line.length) {
+        try {
+          let obj = JSON.parse(line)
+          res.push({
+            type: obj.type,
+            word: obj.typo,
+            lnum: obj.line_num - 1,
+            colStart: obj.byte_offset,
+            colEnd: obj.byte_offset + Buffer.byteLength(obj.typo),
+            corrections: obj.corrections
+          })
+        } catch (e) {
+          // ignored
         }
       }
+    }).then(() => {
       res.sort((a, b) => {
         if (a.lnum != b.lnum) return a.lnum - b.lnum
         return a.colStart - b.colStart
